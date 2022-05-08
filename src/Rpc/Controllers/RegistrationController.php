f78798ac332d2f5520deb31b96583d77c4a97153
Vitaliy <theyamshikov@yandex.ru>
2022-05-08 19:48:07 +0600

    upd

--- src/Rpc/Controllers/RegistrationController.php
+++ src/Rpc/Controllers/RegistrationController.php
@@ -3,7 +3,7 @@
 namespace ZnUser\Registration\Rpc\Controllers;
 
 use Psr\Container\ContainerInterface;
-use ZnCore\Base\Libs\Container\ContainerAwareTrait;
+use ZnCore\Base\Libs\Container\Traits\ContainerAwareTrait;
 use ZnCore\Domain\Helpers\EntityHelper;
 use ZnLib\Rpc\Domain\Entities\RpcRequestEntity;
 use ZnLib\Rpc\Domain\Entities\RpcResponseEntity;
