8b13fd01c70544c2ab735b84198ab2573684534e
Vitaliy <theyamshikov@yandex.ru>
2022-06-26 16:31:22 +0600

    upd

--- src/Domain/Forms/RequestActivationCodeForm.php
+++ src/Domain/Forms/RequestActivationCodeForm.php
@@ -9,7 +9,7 @@ use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Component\Validator\Mapping\ClassMetadata;
 use ZnLib\Components\I18Next\Facades\I18Next;
 use ZnCore\Base\Validation\Interfaces\ValidationByMetadataInterface;
-use ZnLib\Web\Symfony4\MicroApp\Interfaces\BuildFormInterface;
+use ZnLib\Web\Components\Form\Interfaces\BuildFormInterface;
 
 class RequestActivationCodeForm implements ValidationByMetadataInterface, BuildFormInterface
 {
